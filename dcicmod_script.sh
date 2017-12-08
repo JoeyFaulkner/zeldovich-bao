2bc78dd96bd0c129cc71ec01b8abc34eef607d2c
Joey Faulkner <joeyf@roe.ac.uk>
2017-12-08 10:42:29 +0000

    init

--- dcicmod_script.sh
+++ dcicmod_script.sh
@@ -2,8 +2,8 @@
 # Works on my linux machines at work, but the location of various python libraries and include files may differ
 
 #!/bin/bash
-gcc -I/usr/include/python2.6/ -fPIC -fopenmp -c cicdensmodule.c
+gcc -I/usr/include/python2.7/ -fPIC -fopenmp -c cicdensmodule.c
 
-gcc -shared -Xlinker -export-dynamic -I/usr/include/python2.6/ -L/usr/lib/python2.6 -lm cicdensmodule.o -o cicdens.so
+gcc -shared -Xlinker -export-dynamic -I/usr/include/python2.7/ -L/usr/lib/python2.7 -lm cicdensmodule.o -o cicdens.so
 
 
